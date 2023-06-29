1574e9bd0bc9bdddec2a2873ce9891e617adb7d9
theshadowop <128815694+theshadowop@users.noreply.github.com>
2023-06-29 13:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://theshadowop: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@github.com/theshadowop/File-Sharing-Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
